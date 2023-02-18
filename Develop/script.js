c4c11800f885482e6fcee9079dc305c98977d32d
Nestor Montanez <nestormontanez@Nestors-MBP.askey.com>
2023-02-18 00:03:10 -0500

    created button

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,29 @@
 function onload (){
     var btn = document.createElement("button")
-    
+    var container = document.getElementById("div1")
+    console.log(container)
+
+    container.appendChild(btn)
 
 }
-document.body.onload = onload
+document.addEventListener("DOMContentLoaded", onload)
+
+// console.log(window.onload)
 
 
 
-
-
-  // Function to change the content of t2
-function modifyText() {
-    const t2 = document.getElementById("t2");
-    const isNodeThree = t2.firstChild.nodeValue === "three";
-    if (isNodeThree) {
-        t2.firstChild.nodeValue =  "two";
-    } else {
-        t2.firstChild.nodeValue =  "three";
-    }
-  }
+//   // Function to change the content of t2
+// //function modifyText() {
+//     const t2 = document.getElementById("t2");
+//     const isNodeThree = t2.firstChild.nodeValue === "three";
+//     if (isNodeThree) {
+//         t2.firstChild.nodeValue =  "two";
+//     } else {
+//         t2.firstChild.nodeValue =  "three";
+//     }
+//   }
   
-  // Add event listener to table
-  const el = document.getElementById("outside");
-  el.addEventListener("click", modifyText, false);
+//   // Add event listener to table
+//   const el = document.getElementById("outside");
+//   //el.addEventListener("click", modifyText, false);
   
