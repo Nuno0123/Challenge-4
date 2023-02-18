e3bc3636641f4892bdcbc8cc5c55d15c3c394aa2
Nestor Montanez <nestormontanez@Nestors-MBP.askey.com>
2023-02-18 00:43:13 -0500

    removeallchildren

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,25 @@
-function onload (){
-    var btn = document.createElement("button")
-    var container = document.getElementById("div1")
-    console.log(container)
+function startQuiz() {
+    var container = document.getElementById("div1");
+    removeAllChildNodes(container)
 
-    container.appendChild(btn)
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function onload() {
+    var btn = document.createElement("button");
     var newContent = document.createTextNode("Start Quiz");
     btn.appendChild(newContent);
+    btn.addEventListener("click", startQuiz);
+    var container = document.getElementById("div1");
+    container.appendChild(btn);
 
 }
 document.addEventListener("DOMContentLoaded", onload)
-
-// console.log(window.onload)
 
 
 
