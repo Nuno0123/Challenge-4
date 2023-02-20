f4a4f8b2997f7d9e7e2f09c6b057f1c3989ac643
Nestor Montanez <nestormontanez@Nestors-MacBook-Pro.local>
2023-02-19 19:56:57 -0500

    created variables in j.s

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,15 @@
+// Here I am gathering all the elements in my html so I'm able to manipluate them through javascript 
+var Quiz = document.getElementById("quiz");
+var Results = document.getElementById("results");
+var Score = document.getElementById("finalScore");
+var gameOver = document.getElementById("gameOver");
+var quizTimer = document.getElementById("timer");
+var startQuiz = document.getElementById("startButton");
+var beginQuiz = document.getElementById("startQuiz");
+var highScoreCont = document.getElementById("highScoreContainer");
+
+
+// Here I'm gathering my questions for the quiz on my website
 var Questions = [{
     Question: "What does DOM stand for?",
     optionA:"answer",
